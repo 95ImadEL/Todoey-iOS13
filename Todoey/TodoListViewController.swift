49639ece305d1f79940d8dc604e8d37bfb5ed62a
95ImadEL <40009897+95ImadEL@users.noreply.github.com>
2022-01-15 20:42:40 +0100

    Create a textField inside UIAlert to add new items

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class TodoListViewController: UITableViewController{
 
-    let itemArray = ["Read","Eat","Play","Sleep"]
+    var itemArray = ["Read","Eat","Play","Sleep"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -49,6 +49,24 @@ class TodoListViewController: UITableViewController{
         tableView.deselectRow(at: indexPath, animated: true)
     }
     
-
+    // MARK - Add New Items
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+        var textField = UITextField()
+        let alert = UIAlertController(title: "Add New Todoey Item", message: "", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Add Item", style: .default) { action in
+            //What will happen once the user click the add item button on our UIAlert
+        self.itemArray.append(textField.text!)
+        self.tableView.reloadData()
+            
+        }
+        alert.addTextField { alertTextField in
+            alertTextField.placeholder = "Create new Item"
+            textField = alertTextField
+        }
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+    
 }
 
